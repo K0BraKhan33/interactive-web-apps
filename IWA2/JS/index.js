4044655647d35bf25aa44df730573a2e971346d0
K0BraKhan33 <mikaeel.gamieldien@gmail.com>
2023-06-20 16:43:31 +0200

    new file:   IWA2/JS/index.js     new file:   IWA2/index.html

---
+++ IWA2/JS/index.js
@@ -0,0 +1,42 @@
+/*    1     */
+
+/* This is the firstname of the user */
+
+const user = 'John'
+
+
+/* This is the lastname of the user */
+ const surname = 'Smith'
+
+
+console.log(user+' '+surname)
+
+
+/*    2    */ 
+
+
+/** (c) ACME Inc. 2010 */
+
+/*
+ * This date Cannot be changed by a user or computer to their account 
+ */
+const date = '10/07/2014'
+
+console.log(date)
+
+
+
+/*  3   */
+
+/* 
+It is important to show the following message in order to:
+- to assure users
+- to scare hackers
+- to impress investors
+*/
+
+console.warn("Security scan starting")
+
+/* It is important to let user know when they can close the page  */
+
+console.info("Please wait for scan to complete before closing the browser.")
