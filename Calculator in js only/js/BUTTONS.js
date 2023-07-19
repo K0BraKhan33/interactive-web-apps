996c097bf6629fd3202583b5fec98fc20d1b6ea3
K0BraKhan33 <mikaeel.gamieldien@gmail.com>
2023-07-19 08:55:11 +0200

    all

---
+++ Calculator in js only/js/BUTTONS.js
@@ -0,0 +1,170 @@
+  function button1() {
+    let buttonAdd = document.createElement('button');
+    let buttonText = document.createTextNode('+');
+    let buttonId = 'b1';
+    
+    buttonAdd.appendChild(buttonText);
+    document.body.appendChild(buttonAdd);
+    buttonAdd.setAttribute('id', buttonId);
+  }
+  function button2() {
+    let buttonSubtract = document.createElement('button');
+    let buttonText = document.createTextNode('-');
+    let buttonId = 'b2';
+    
+    buttonSubtract.appendChild(buttonText);
+    document.body.appendChild(buttonSubtract);
+    buttonSubtract.setAttribute('id', buttonId);
+  }
+  
+  function button3() {
+    let buttonEquals = document.createElement('button');
+    let buttonText = document.createTextNode('=');
+    let buttonId = 'b3';
+    
+    buttonEquals.appendChild(buttonText);
+    document.body.appendChild(buttonEquals);
+    buttonEquals.setAttribute('id', buttonId);
+  }
+  function button4() {
+    let buttonMultiply = document.createElement('button');
+    let buttonText = document.createTextNode('×');
+    let buttonId = 'b4';
+    
+    buttonMultiply.appendChild(buttonText);
+    document.body.appendChild(buttonMultiply);
+    buttonMultiply.setAttribute('id', buttonId);
+  }
+  
+  function button5() {
+    let buttonDivide = document.createElement('button');
+    let buttonText = document.createTextNode('÷');
+    let buttonId = 'b5';
+    
+    buttonDivide.appendChild(buttonText);
+    document.body.appendChild(buttonDivide);
+    buttonDivide.setAttribute('id', buttonId);
+  }
+  function button6() {
+    let buttonNumber = document.createElement('button');
+    let buttonText = document.createTextNode('1');
+    let buttonId = 'b6';
+    
+    buttonNumber.appendChild(buttonText);
+    document.body.appendChild(buttonNumber);
+    buttonNumber.setAttribute('id', buttonId);
+  }
+  function button7() {
+    let buttonNumber = document.createElement('button');
+    let buttonText = document.createTextNode('2');
+    let buttonId = 'b7';
+    
+    buttonNumber.appendChild(buttonText);
+    document.body.appendChild(buttonNumber);
+    buttonNumber.setAttribute('id', buttonId);
+  }
+  function button8() {
+    let buttonNumber = document.createElement('button');
+    let buttonText = document.createTextNode('3');
+    let buttonId = 'b8';
+    
+    buttonNumber.appendChild(buttonText);
+    document.body.appendChild(buttonNumber);
+    buttonNumber.setAttribute('id', buttonId);
+  }
+  function button9() {
+    let buttonNumber = document.createElement('button');
+    let buttonText = document.createTextNode('4');
+    let buttonId = 'b9';
+    
+    buttonNumber.appendChild(buttonText);
+    document.body.appendChild(buttonNumber);
+    buttonNumber.setAttribute('id', buttonId);
+  }
+  function button10() {
+    let buttonNumber = document.createElement('button');
+    let buttonText = document.createTextNode('5');
+    let buttonId = 'b10';
+    
+    buttonNumber.appendChild(buttonText);
+    document.body.appendChild(buttonNumber);
+    buttonNumber.setAttribute('id', buttonId);
+  }
+  function button11() {
+    let buttonNumber = document.createElement('button');
+    let buttonText = document.createTextNode('6');
+    let buttonId = 'b11';
+    
+    buttonNumber.appendChild(buttonText);
+    document.body.appendChild(buttonNumber);
+    buttonNumber.setAttribute('id', buttonId);
+  }
+  function button12() {
+    let buttonNumber = document.createElement('button');
+    let buttonText = document.createTextNode('7');
+    let buttonId = 'b12';
+    
+    buttonNumber.appendChild(buttonText);
+    document.body.appendChild(buttonNumber);
+    buttonNumber.setAttribute('id', buttonId);
+  }
+  function button13() {
+    let buttonNumber = document.createElement('button');
+    let buttonText = document.createTextNode('8');
+    let buttonId = 'b13';
+    
+    buttonNumber.appendChild(buttonText);
+    document.body.appendChild(buttonNumber);
+    buttonNumber.setAttribute('id', buttonId);
+  }
+  function button14() {
+    let buttonNumber = document.createElement('button');
+    let buttonText = document.createTextNode('9');
+    let buttonId = 'b14';
+    
+    buttonNumber.appendChild(buttonText);
+    document.body.appendChild(buttonNumber);
+    buttonNumber.setAttribute('id', buttonId);
+  }
+  function button15() {
+    let buttonNumber = document.createElement('button');
+    let buttonText = document.createTextNode('0');
+    let buttonId = 'b15';
+    
+    buttonNumber.appendChild(buttonText);
+    document.body.appendChild(buttonNumber);
+    buttonNumber.setAttribute('id', buttonId);
+  }
+
+
+  button1();
+  button2();
+  button3();
+  button4();
+  button5();
+  button6();
+  button7();
+  button8();
+  button9();
+  button10();
+  button11();
+  button12();
+  button13();
+  button14();
+  button15();
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- Calculator in js only/js/index.js
+++ Calculator in js only/js/index.js
@@ -1,2 +1,175 @@
-let add=document.createAttribute('BUTTON')
-let bText=document.createAttribute("button")
+
+
+let inum = 0;
+let sNum = '';
+let iTotal = 0;
+
+function num1(){
+    sNum=sNum+'1';
+     inum=parseFloat(sNum);
+     console.log(inum)
+}
+function num2(){
+    sNum=sNum+'2';
+    inum=parseFloat(sNum);
+    console.log(inum)
+}
+
+function num3(){
+    sNum=sNum+'3';
+    inum=parseFloat(sNum);
+    console.log(inum)
+}
+function num4(){
+    sNum=sNum+'4';
+    inum=parseFloat(sNum);
+    console.log(inum)
+}
+function num5(){
+    sNum=sNum+'5';
+    inum=parseFloat(sNum);
+    console.log(inum)
+}
+function num6(){
+    sNum=sNum+'6';
+    inum=parseFloat(sNum);
+    console.log(inum)
+}
+function num7(){
+    sNum=sNum+'7';
+    inum=parseFloat(sNum);
+    console.log(inum)
+  }
+function num8(){
+    sNum=sNum+'8';
+    inum=parseFloat(sNum);
+    console.log(inum)
+}
+function num9(){
+    sNum=sNum+'9';
+    inum=parseFloat(sNum);
+    console.log(inum)
+}
+function num0(){
+    sNum=sNum+'0';
+    inum=parseFloat(sNum);
+    console.log(inum)
+}
+
+
+const number1=document.getElementById('b6');
+number1.addEventListener('click',num1);
+const number2=document.getElementById('b7');
+number2.addEventListener('click',num2);
+const number3=document.getElementById('b8');
+number3.addEventListener('click',num3);
+const number4=document.getElementById('b9');
+number4.addEventListener('click',num4);
+const number5=document.getElementById('b10');
+number5.addEventListener('click',num5);
+const number6=document.getElementById('b11');
+number6.addEventListener('click',num6);
+const number7=document.getElementById('b12');
+number7.addEventListener('click',num7);
+const number8=document.getElementById('b13');
+number8.addEventListener('click',num8);
+const number9=document.getElementById('b14');
+number9.addEventListener('click',num9);
+const number0=document.getElementById('b15');
+number0.addEventListener('click',num0);
+
+function calculate(){
+    console.log(iTotal);
+    inum=0
+}
+function ac(){
+     inum=0;
+     sNum='';
+     iTotal=0;
+    console.log(iTotal);
+}
+function plus(){
+
+    iTotal=iTotal+inum;
+    console.log('total is now:'+ iTotal +' was added:'+inum)
+    console.log('+');
+    calculate();
+    console.log(iTotal);
+    inum=0
+    sNum=''
+}
+
+
+
+function subtract(){
+
+    iTotal=iTotal-inum;
+    console.log('total is now:'+ iTotal +' was added:'+inum)
+    console.log('-');
+    calculate();
+    console.log(iTotal);
+    inum=0
+    sNum=''
+}
+
+
+function total(){
+
+    iTotal=iTotal;
+    console.log('total is now:'+ iTotal +' was added:'+inum)
+    console.log('=');
+    calculate();
+    console.log(iTotal);
+    inum=0
+    sNum=''
+}
+
+
+function multiply(){
+
+    iTotal=iTotal*inum;
+    console.log('total is now:'+ iTotal +' was added:'+inum)
+    console.log('*');
+    calculate();
+    console.log(iTotal);
+    inum=0
+    sNum=''
+}
+
+function devide(){
+
+    iTotal=iTotal/inum;
+    console.log('total is now:'+ iTotal +' was devided:'+inum)
+    console.log('/');
+    calculate();
+    console.log(iTotal);
+    inum=0
+    sNum=''
+}
+function plus1(){
+    p=true;
+    m=false;
+    t=false;
+    d=false
+}
+
+
+
+
+
+const Add=document.getElementById('b1');
+Add.addEventListener('click',plus);
+
+const Minus=document.getElementById('b2');
+Minus.addEventListener('click',subtract);
+
+const equals=document.getElementById('b3');
+equals.addEventListener('click',total);
+
+const times=document.getElementById('b4');
+times.addEventListener('click',multiply);
+
+const devides=document.getElementById('b5');
+devides.addEventListener('click',devide);
+
+
